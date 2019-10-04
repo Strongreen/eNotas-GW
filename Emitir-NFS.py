a2cc1460e3c449f9841cfedbefbdc4f53128114b
Hérika Ströngreen <ka.ah19@hotmail.com>
2019-10-04 10:35:39 -0300

    Update Emitir-NFS.py

--- Emitir-NFS.py
+++ Emitir-NFS.py
@@ -44,10 +44,8 @@ for x in data:
       "valorTotal": x['valorTotal']
 }
 
-
-
-  APIKEY = 'MzEwMzdmOTctY2E5Ni00NDkzLWIxYTUtNTcwZGJmNDcwMTAw'
-  Empresa_ID = ''
+  APIKEY = 'Inserir A api Key'
+  Empresa_ID = 'ID da empresa'
 
   url = "https://api.enotasgw.com.br/v2/empresas/" + Empresa_ID + "/nf-e"
 
